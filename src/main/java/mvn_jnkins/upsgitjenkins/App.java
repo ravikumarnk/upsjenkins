9fe7eb6027659a6bd46b93e330821cd70bad3cde
sc <sc@host.docker.internal>
2023-12-04 11:20:21 +0530

    first commit

---
+++ src/main/java/mvn_jnkins/upsgitjenkins/App.java
@@ -0,0 +1,17 @@
+package mvn_jnkins.upsgitjenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "executing from main... !" );
+    }
+    
+    
+    
+}
---
+++ src/test/java/mvn_jnkins/upsgitjenkins/AppTest.java
@@ -0,0 +1,23 @@
+package mvn_jnkins.upsgitjenkins;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        
+        System.out.println("testing invoked");
+        
+    }
+}
